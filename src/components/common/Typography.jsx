44eab612f3e550deb368801ad1da1ea788cfee1f
Alina <alinadakhno60@gmail.com>
2022-06-06 14:37:06 +0200

    add typography

---
+++ src/components/common/Typography.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import COLORS from "./Colors";
+// @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+export const H1 = styled.h1`
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 42px;
+`;
+export const H2 = styled.h2`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;700&display=swap");
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 28px;
+  line-height: 37px;
+`;
+
+export const H3 = styled.h3`
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap");
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+`;
+
+export const H4 = styled.h4`
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+`;
+export const H5 = styled.h5`
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 21px;
+`;
+export const H6 = styled.h6`
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  color: ${COLORS.orange};
+`;
+export const RegularP = styled.p`
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+`;
+export const BoldP = styled.p`
+  font-family: "Roboto Slab";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 24px;
+`;
+export const MonoP = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+    font-family: 'Roboto Mono', monospace;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+`;
