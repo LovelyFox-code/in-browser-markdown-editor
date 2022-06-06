09d2f9e5e2095c0603508fb3691f6cd35c451d8d
Alina <alinadakhno60@gmail.com>
2022-06-06 14:14:55 +0200

    create color components

---
+++ src/components/common/Colors.jsx
@@ -0,0 +1,14 @@
+export const COLORS = {
+    darkBlack: '#151619',
+    black: '#1D1F22',
+    lightBlack: '#2B2D31',
+    lighterBlack: '#35393F',
+    darkGray: '#5A6069',
+    gray: '#7C8187',
+    lightGray: '#C1C4CB',
+    lighterGray: '#E4E4E4',
+    dirtyWhite: '#F5F5F5',
+    white: '#FFFFFF',
+    orange: '#E46643',
+    peach: '#F39765'
+}
