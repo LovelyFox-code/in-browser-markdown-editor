0259ebc9bd5ad382c6a05ea8911322b31c1cff81
Alina <alinadakhno60@gmail.com>
2022-06-09 17:04:48 +0200

    css correct

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import EditorContainer from "./components/EditorContainer/EditorContainer";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import React from "react";
-
+import data from '../src/data/data.json'
 
 
 function App() {
   const style = {
-    width: "100%"
+    width: "1440px",
+    overflow: "hidden"
+
   }
   const flex = {
     display: "flex"
   }
-
+ 
   return (
     <div style={flex}>
-       <Sidebar/>
+       <Sidebar files = {data}/>
         <div style={style}>
         <Navbar>
         </Navbar>
-        <EditorContainer/>
+        <EditorContainer markdownText = {data}/>
         </div>
 
     </div>
