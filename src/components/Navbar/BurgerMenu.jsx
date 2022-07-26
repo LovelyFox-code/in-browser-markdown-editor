e242c4fece5eb52005c6d8dcfc3478094a6c95f4
Alina <alinadakhno60@gmail.com>
2022-07-26 12:57:48 +0200

    fill-rule to fillRule

--- src/components/Navbar/BurgerMenu.jsx
+++ src/components/Navbar/BurgerMenu.jsx
@@ -7,14 +7,14 @@ export default function BurgerMenu(props) {
     <div onClick={props.handleToggle}>
       {props.open ? (
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-          <g fill="#FFF" fill-rule="evenodd">
+          <g fill="#FFF" fillRule="evenodd">
             <path d="M2.1.686 23.315 21.9l-1.415 1.415L.686 2.1z" />
             <path d="M.686 21.9 21.9.685l1.415 1.415L2.1 23.314z" />
           </g>
         </svg>
       ) : (
         <svg width="30" height="18" xmlns="http://www.w3.org/2000/svg">
-          <g fill="#FFF" fill-rule="evenodd">
+          <g fill="#FFF" fillRule="evenodd">
             <path d="M0 0h30v2H0zM0 8h30v2H0zM0 16h30v2H0z" />
           </g>
         </svg>
